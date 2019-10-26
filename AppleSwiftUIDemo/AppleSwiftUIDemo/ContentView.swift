0805d9fc9bca2f6109c1dc7470883370cedc127c
Pratik Hirve <hirve.pratik1@gmail.com>
2019-10-26 21:18:35 +0530

    Initial Commit

---
+++ AppleSwiftUIDemo/AppleSwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleSwiftUIDemo
+//
+//  Created by Pratik Hirve on 26/10/19.
+//  Copyright © 2019 Pratik Hirve. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
